b9933ad5aee007b8a75c326dc2fd73fb5a7c1153
Bill Ni <bill.8ni@gmail.com>
2022-02-01 14:30:43 -0500

    screen grab into window

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+
+sct = mss()
+
+cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+
+countdown = 400
+
+while countdown > 0:
+	sct_img = sct.grab(sct.monitors[1])
+	cv2.imshow('Live', np.array(sct_img))
+	cv2.resizeWindow("Live", 480, 270)
+
+	countdown -= 1
+	cv2.waitKey(1)
+
+
+cv2.destroyAllWindows()
