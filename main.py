93713890d391d1509224f74cb806f924e41e6127
Bill Ni <bill.8ni@gmail.com>
2022-02-01 14:50:28 -0500

    refactor into "main" function

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+"""temporary main docstring"""
+
 import numpy as np
 import cv2
 from mss import mss
 
-sct = mss()
+def main():
 
-cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+    sct = mss()
 
-countdown = 400
+    cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 
-while countdown > 0:
-    sct_img = sct.grab(sct.monitors[1])
-    cv2.imshow('Live', np.array(sct_img))
-    cv2.resizeWindow("Live", 480, 270)
+    countdown = 400
 
-    countdown -= 1
-    cv2.waitKey(1)
+    while countdown > 0:
+        sct_img = sct.grab(sct.monitors[1])
+        cv2.imshow('Live', np.array(sct_img))
+        cv2.resizeWindow("Live", 480, 270)
 
+        countdown -= 1
+        cv2.waitKey(1)
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
