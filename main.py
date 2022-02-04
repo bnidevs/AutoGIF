3d3bf302e7fe48bd2e1fe2f4bb01458062900fa5
henry <henrycullom@gmail.com>
2022-02-04 17:17:25 -0500

    Added a bounding box that gets users screen size

--- main.py
+++ main.py
@@ -3,18 +3,27 @@
 import numpy as np
 import cv2
 from mss import mss
+from PIL import Image
 
 def main():
     """main function"""
     sct = mss()
+    # take a screenshot of the screen and use that to determine the user's screen size
+    sizing_sct = sct.grab(sct.monitors[1])
+    box = {'left': 0, 'top': 0, 'width': sizing_sct.width, 'height': sizing_sct.height}
 
     cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 
     countdown = 400
 
     while countdown > 0:
-        sct_img = sct.grab(sct.monitors[1])
-        cv2.imshow('Live', np.array(sct_img))
+        sct_img = sct.grab(box)
+        img = Image.frombytes(
+            'RGB', 
+            (sct_img.width, sct_img.height), 
+            sct_img.rgb, 
+        )
+        cv2.imshow('Live', np.array(img))
         cv2.resizeWindow("Live", 480, 270)
 
         countdown -= 1
