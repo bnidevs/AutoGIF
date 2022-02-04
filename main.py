b1428806ea6b32796f855419a46c8b7a3a703473
henry <henrycullom@gmail.com>
2022-02-04 17:45:38 -0500

    added basic zoom in and out method

--- main.py
+++ main.py
@@ -5,12 +5,25 @@ import cv2
 from mss import mss
 from PIL import Image
 
+box = {'left': 0, 'top': 0, 'width': 200, 'height': 200}
+
+# add some way to zoom based on aspect ratio instead of arbitrary values
+def zoom_in(speed):
+	box['left'] += 2*speed
+	box['top'] += speed
+	box['width'] -= 4*speed
+	box['height'] -= 2*speed
+
+def zoom_out(speed):
+	zoom_in(-1 * speed)
+
 def main():
     """main function"""
     sct = mss()
     # take a screenshot of the screen and use that to determine the user's screen size
     sizing_sct = sct.grab(sct.monitors[1])
-    box = {'left': 0, 'top': 0, 'width': sizing_sct.width, 'height': sizing_sct.height}
+    box['width'] = sizing_sct.width
+    box['height'] = sizing_sct.height
 
     cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 
@@ -25,7 +38,10 @@ def main():
         )
         cv2.imshow('Live', np.array(img))
         cv2.resizeWindow("Live", 480, 270)
-
+        
+        if countdown % 2 == 0:
+        	zoom_in(1)
+        
         countdown -= 1
         cv2.waitKey(1)
 
