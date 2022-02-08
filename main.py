696b8fc00bb9f53fb92c8089c9a69634d1952096
henry <henrycullom@gmail.com>
2022-02-08 16:40:08 -0500

    added aspect ratio function for more accurate zoom

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ from PIL import Image
 
 box = {'left': 0, 'top': 0, 'width': 200, 'height': 200}
 
-def zoom_in(speed):
-    """zoom based on aspect ratio instead of arbitrary values"""
-    box['left'] += 2*speed
-    box['top'] += speed
-    box['width'] -= 4*speed
-    box['height'] -= 2*speed
+def gcd(a,b):
+	if b == 0:
+		return a
+	return gcd(b, a % b)
 
-def zoom_out(speed):
+def get_aspect_ratio(width, height):
+	factor = gcd(width, height)
+	return (width / factor, height / factor)
+
+def zoom_in(speed, aspect_ratio):
+    """zoom in based on aspect ratio"""
+    box['width'] -= aspect_ratio[0] * speed
+    box['height'] -= aspect_ratio[1] * speed
+
+def zoom_out(speed, aspect_ratio):
     """opposite of zoom in"""
     zoom_in(-1 * speed)
 
@@ -26,10 +33,11 @@ def main():
     # take a screenshot of the screen and use that to determine the user's screen size
     sizing_sct = sct.grab(sct.monitors[1])
     box = {'left': 0, 'top': 0, 'width': sizing_sct.width, 'height': sizing_sct.height}
+    aspect_ratio = get_aspect_ratio(sizing_sct.width, sizing_sct.height)
 
     cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 
-    countdown = 400
+    countdown = 800
 
     while countdown > 0:
         sct_img = sct.grab(box)
@@ -39,10 +47,7 @@ def main():
             sct_img.rgb,
         )
         cv2.imshow('Live', np.array(img))
-        cv2.resizeWindow("Live", 480, 270)
-
-        if countdown % 2 == 0:
-            zoom_in(1)
+        cv2.resizeWindow("Live", width, height)
 
         countdown -= 1
         cv2.waitKey(1)
