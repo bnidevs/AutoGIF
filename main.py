d5bcb81b021bc2519c48ed8a2c6a637aa1b1bffe
henry <henrycullom@gmail.com>
2022-02-08 17:20:12 -0500

    readded aspect_ratio function for future use, press q to quit added

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ from PIL import Image
 
 box = {'left': 0, 'top': 0, 'width': 200, 'height': 200}
 
+def gcd(a,b):
+	if b == 0:
+		return a
+	return gcd(b, a % b)
+
+def get_aspect_ratio(width, height):
+	factor = gcd(width, height)
+	return (width / factor, height / factor)
+
 def main():
     """main function"""
     sct = mss()
     # take a screenshot of the screen and use that to determine the user's screen size
     sizing_sct = sct.grab(sct.monitors[1])
-    box = {'left': 0, 'top': 0, 'width': sizing_sct.width, 'height': sizing_sct.height}
+    scr_width = sizing_sct.width
+    scr_height = sizing_sct.height
+    box = {'left': 0, 'top': 0, 'width': scr_width, 'height': scr_height}
+    aspect_ratio = get_aspect_ratio(scr_width, scr_height)
 
     cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 
@@ -28,10 +40,11 @@ def main():
             sct_img.rgb,
         )
         cv2.imshow('Live', np.array(img))
-        cv2.resizeWindow("Live", width, height)
+        cv2.resizeWindow("Live", 720, 480)
 
         countdown -= 1
-        cv2.waitKey(1)
+        if cv2.waitKey(1) == ord('q'):
+        	break
 
     cv2.destroyAllWindows()
 
