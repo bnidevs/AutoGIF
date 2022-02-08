4cbab1ab399beb40d5ab7d708293b0e6f36cc55f
Bill Ni <bill.8ni@gmail.com>
2022-02-08 18:12:01 -0500

    add videowriter + threading

--- main.py
+++ main.py
@@ -1,24 +1,67 @@
 """temporary file docstring"""
 
+# pylint: disable=redefined-outer-name, global-statement
+
+from threading import Thread
 import numpy as np
 import cv2
 from mss import mss
 
+FRAME_LIST = []
+RECORDING = True
+
+OUT = cv2.VideoWriter('test.avi', cv2.VideoWriter_fourcc(*"XVID"), 60, (1920, 1080))
+
+def save_frame():
+    """function for frame writing thread"""
+    count = 0
+
+    while RECORDING or len(FRAME_LIST) > 0:
+        if len(FRAME_LIST) > 0:
+            print(count)
+            count += 1
+
+            current_frame = FRAME_LIST.pop(0)
+
+            OUT.write(current_frame)
+
+    OUT.release()
+
 def main():
     """main function"""
+    global RECORDING
+
+    writer_thread = Thread(target = save_frame)
+
     sct = mss()
 
     cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
 
-    countdown = 400
+    countdown = 300
+
+    writer_thread.start()
 
     while countdown > 0:
         sct_img = sct.grab(sct.monitors[1])
-        cv2.imshow('Live', np.array(sct_img))
+
+        frame = np.array(sct_img)
+
+        frame = cv2.resize(frame, (1920, 1080))
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        FRAME_LIST.append(frame)
+
+        cv2.imshow('Live', frame)
         cv2.resizeWindow("Live", 480, 270)
 
         countdown -= 1
-        cv2.waitKey(1)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    RECORDING = False
+
+    writer_thread.join()
 
     cv2.destroyAllWindows()
 
