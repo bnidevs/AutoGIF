90b8e221d87c8cc0fb47a58f6aac2ce763484fb5
Bill Ni <bill.8ni@gmail.com>
2022-02-12 11:21:13 -0500

    add initial mac test project for menu bar

---
+++ AutoGIF Spooler/AutoGIF Spooler/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  AutoGIF Spooler
+//
+//  Created by Bill Ni on 2/12/22.
+//
+
+import Foundation
+import Cocoa
+import SwiftUI
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var statusBar: StatusBarController?
+    var popover = NSPopover.init()
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let contentView = ContentView()
+        popover.contentSize = NSSize(width: 360, height: 360)
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        statusBar = StatusBarController.init(popover)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
---
+++ AutoGIF Spooler/AutoGIF Spooler/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AutoGIF Spooler
+//
+//  Created by Bill Ni on 2/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
