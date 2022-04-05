ee35094f567745a3408dc7ccdfd35e4e5e04e2a3
NMiklu <Sinntheskiller@gmail.com>
2022-04-05 17:18:58 -0400

    Combined gui 'App' class -> Has a window to set delay and record time.

--- main.py
+++ main.py
@@ -3,23 +3,23 @@
 import numpy as np
 import cv2
 from mss import mss
+from guiTest import App
 
 def main():
     """main function"""
     sct = mss()
-
-    cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
-
-    countdown = 400
-
-    while countdown > 0:
+    
+    cv2.namedWindow("Preview", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Preview", 480,270)
+    while True:
         sct_img = sct.grab(sct.monitors[1])
-        cv2.imshow('Live', np.array(sct_img))
-        cv2.resizeWindow("Live", 480, 270)
-
-        countdown -= 1
-        cv2.waitKey(1)
-
+        cv2.imshow("Preview", np.array(sct_img))
+        
+        if cv2.waitKey(1) == ord('q'):
+            break
+        if cv2.getWindowProperty('Preview',cv2.WND_PROP_VISIBLE) == 0:
+            break
+        
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
--- test_gif_record.py
+++ test_gif_record.py
@@ -5,57 +5,67 @@ import cv2
 from mss import mss
 import imageio
 import time
-
+import guiTest as gui
+import tkinter as tnk
 
 FRAME_LIST = []
-maxRecordTime = 1
-FPS = 10
-delay = 1;
+MAXRECORDTIME = 1
+FPS = 30
+DELAY = 1
+master = tnk.Tk()
+master.title('AutoGIF')
+master.geometry('320x180')
 
 def windowLoop(sct):
-    cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('Live',720,480)
-    cv2.createButton("Start",startButton,None,cv2.QT_PUSH_BUTTON,0)
-    cv2.createTrackbar('Seconds', 'Live', 1, 30, onSecondsChange)
-    cv2.createTrackbar('Delay', 'Live', 1, 30, onDelayChange)
-    while(cv2.getWindowProperty('Live', cv2.WND_PROP_VISIBLE) != 0):
+    global master
+    global MAXRECORDTIME
+    global DELAY
+    ap = gui.App(master=master)
+    ap.run()
+    MAXRECORDTIME = ap.recordTime
+    DELAY = ap.delayTime
+    cv2.namedWindow("Preview")
+    cv2.resizeWindow('Preview',720,480)
+    
+    
+    while(cv2.getWindowProperty('Preview', cv2.WND_PROP_VISIBLE) != 0):
         img = sct.grab(sct.monitors[1])
         frame = np.array(img)
         frame = cv2.resize(frame,(720,480))
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        cv2.imshow('Live',frame)
-        cv2.waitKey(1)
-
-
-def startButton():
-    cv2.destroyAllWindows()
-
-def onSecondsChange(value):
-    maxRecordTime = value
-def onDelayChange(value):
-    delay = value
+        cv2.imshow('Preview',frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 def convertToGif(lst):
+    global FPS
+    global FRAME_LIST
     for x in lst:
         frame = np.array(x)
         frame = cv2.resize(frame, (720,480))
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         FRAME_LIST.append(frame)
-    imageio.mimsave('test.gif', FRAME_LIST, fps=FPS)
+    imageio.mimsave('output.gif', FRAME_LIST, fps=FPS)
 
 def main():
     """main function"""
+    global FPS
+    global DELAY
+    global MAXRECORDTIME
+    global FRAME_LIST
     sct_img_list = []
     sct = mss()
     windowLoop(sct)
-    
 
-
-    countdown = maxRecordTime*FPS
+    countdown = MAXRECORDTIME*FPS
     cv2.destroyAllWindows()
-    time.sleep(delay)
+    while DELAY > 0:
+        time.sleep(1)
+        print(DELAY)
+        DELAY -= 1
+    print("\nRecording...\n")
     while countdown > 0:
         sct_img = sct.grab(sct.monitors[1])
         sct_img_list.append(sct_img)
@@ -64,9 +74,7 @@ def main():
     
     print("\nConverting to GIF...\n")
     convertToGif(sct_img_list)
-    
-    
-    
+    print("\nRecording time: ", MAXRECORDTIME)
 
 if __name__ == "__main__":
     main()
